ca73fc5857a5e19012bcc4ff3db4c8da0441d832
dwaipayan bhattacharya <rxdwaipayan@gmail.com>
2019-06-12 09:49:56 +0530

    new changes in api-validation

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿
+using DatingApp.API.Data;
+using DatingApp.API.DTO;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthRepository repo;
+        public AuthController(IAuthRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegister)
+        {
+            userForRegister.username = userForRegister.username.ToLower();
+            if (await repo.UserExist(userForRegister.username))
+            {
+                return BadRequest("Username already exist");
+            }
+            var userToCreate = new User
+            {
+                Name = userForRegister.username
+            };
+
+            var userCreated = await repo.Register(userToCreate, userForRegister.password);
+
+            return StatusCode(201);
+
+        }
+    }
+}
---
+++ DatingApp.API/DTO/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.DTO
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        [StringLength(8,MinimumLength =4, ErrorMessage ="Must be within 8 to 4 letters")]
+        public string password { get; set; }
+    }
+}
